23a325956e26f97f814b01da66d5b7029a7eec1a
Natalie Fearnley <nfearnley@gmail.com>
2021-08-29 14:50:11 -0400

    Updated code to work with discordn 1.2.2

--- schedulebot/lib/logger.py
+++ schedulebot/lib/logger.py
@@ -28,7 +28,7 @@ def init_logging():
 
 
 def create_log_levels():
-    global BANNER, LOGIN, EGG, CMD, ROYALE
+    global BANNER, LOGIN, EGG, CMD
     BANNER = digilogger.addLogLevel("banner", fg="orange_red_1", bg="deep_sky_blue_4b", attr="bold", showtime = False)
     LOGIN = digilogger.addLogLevel("login", fg="cyan")
     EGG = digilogger.addLogLevel("egg", fg="magenta_2b", bg="light_yellow", attr="bold", prefix="EGG")
--- schedulebot/main.py
+++ schedulebot/main.py
@@ -1,7 +1,5 @@
 
-import os
 import sys
-from datetime import datetime
 import logging
 import digiformatter.styles
 
@@ -12,7 +10,6 @@ import discordn
 
 from schedulebot import __version__
 from schedulebot.conf import ConfLoadException, conf, load_conf
-from schedulebot.lib import paths
 from schedulebot.lib.logger import init_logging, BANNER, CMD, LOGIN
 from schedulebot.lib.utils import truncate
 
@@ -20,20 +17,33 @@ init_logging()
 
 discordn.patch()
 
-logger = logging.getLogger("schedulebot")
-initial_cogs = ["calendar"]
-initial_extensions = []
+logger = logging.getLogger(__package__)
 
 
-def initConf():
-    print("Initializing configuration file")
-    try:
-        conf.init()
-        print(f"Configuration file initialized: {paths.confpath}")
-    except FileExistsError as e:
-        print(e)
-        pass
-    os.startfile(paths.confpath.parent)
+class ScheduleBot(Bot):
+    async def on_first_ready(self):
+        await super().on_first_ready()
+
+        # Print the splash screen.
+        # Obviously we need the banner printed in the terminal
+        banner = ("ScheduleBot " + __version__)
+        logger.log(BANNER, banner)
+        logger.log(LOGIN, f"Logged in as: {self.user.name} ({self.user.id})\n------")
+
+        # More splash screen.
+        print(digiformatter.styles)
+        logger.info(f"Prefix: {conf.prefix}")
+        logger.debug(f"ScheduleBot launched in {round((self.load_time.total_seconds() * 1000), 3)} milliseconds.\n")
+
+    async def on_reconnect_ready(self):
+        logger.error("ScheduleBot has been reconnected to Discord.")
+
+    async def on_command(self, ctx):
+        guild = truncate(ctx.guild.name, 20) if (hasattr(ctx, "guild") and ctx.guild is not None) else "DM"
+        logger.log(CMD, f"G {guild}, U {ctx.message.author.name}: {ctx.message.content}")
+
+    def on_disconnect(self):
+        logger.error("ScheduleBot has been disconnected from Discord!")
 
 
 def main():
@@ -42,76 +52,23 @@ def main():
     except ConfLoadException:
         return
 
-    launchtime = datetime.now()
+    extensions = []
+    cogs = ["calendar"]
+    extension_paths = [f"{__package__}.cogs.{c}" for c in cogs] + [f"{__package__}.extension.{e}" for e in extensions]
 
-    bot = Bot(command_prefix = conf.prefix)
+    # Add a special message to bot status if we are running in debug mode
+    # todo: add to discordn
+    activity = discord.Game(name = "with the timeline")
+    if sys.gettrace() is not None:
+        activity = discord.Activity(type=discord.ActivityType.listening, name = "DEBUGGER 🔧")
 
-    for extension in initial_extensions:
-        bot.load_extension("schedulebot.extensions." + extension)
-    for cog in initial_cogs:
-        bot.load_extension("schedulebot.cogs." + cog)
-
-    @bot.event
-    async def on_first_ready():
-        # Set the bots name to what's set in the config.
-        try:
-            await bot.user.edit(username = conf.name)
-        except discord.errors.HTTPException:
-            logger.warn("We can't change the username this much!")
-
-        # Print the splash screen.
-        # Obviously we need the banner printed in the terminal
-        banner = ("ScheduleBot " + __version__)
-        logger.log(BANNER, banner)
-        logger.log(LOGIN, f"Logged in as: {bot.user.name} ({bot.user.id})\n------")
-
-        # Add a special message to bot status if we are running in debug mode
-        activity = discord.Game(name = "with the timeline")
-        if sys.gettrace() is not None:
-            activity = discord.Activity(type=discord.ActivityType.listening, name = "DEBUGGER 🔧")
-
-        # More splash screen.
-        await bot.change_presence(activity = activity)
-        print(digiformatter.styles)
-        logger.info(f"Prefix: {conf.prefix}")
-        launchfinishtime = datetime.now()
-        elapsed = launchfinishtime - launchtime
-        logger.debug(f"ScheduleBot launched in {round((elapsed.total_seconds() * 1000), 3)} milliseconds.\n")
-
-    @bot.event
-    async def on_reconnect_ready():
-        logger.error("ScheduleBot has been reconnected to Discord.")
-
-    @bot.event
-    async def on_command(ctx):
-        guild = truncate(ctx.guild.name, 20) if (hasattr(ctx, "guild") and ctx.guild is not None) else "DM"
-        logger.log(CMD, f"G {guild}, U {ctx.message.author.name}: {ctx.message.content}")
-
-    @bot.event
-    async def on_message(message):
-        # F*** smart quotes.
-        message.content = message.content.replace("“", "\"")
-        message.content = message.content.replace("”", "\"")
-        message.content = message.content.replace("’", "'")
-        message.content = message.content.replace("‘", "'")
-
-        await bot.process_commands(message)
-
-    @bot.event
-    async def on_message_edit(before, after):
-        if before.content == after.content:
-            return
-        await bot.process_commands(after)
-
-    def on_disconnect():
-        logger.error("ScheduleBot has been disconnected from Discord!")
+    bot = ScheduleBot(command_prefix = conf.prefix, extensions=extension_paths, username=conf.name, activity=activity)
 
     if not conf.authtoken:
         logger.error("Authentication token not found!")
         return
 
     bot.run(conf.authtoken)
-    on_disconnect()
 
 
 if __name__ == "__main__":
